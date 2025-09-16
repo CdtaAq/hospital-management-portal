4d52b21423901c3d42587efee7feafb2e2309110
chris Taylor <ctay456a@gmail.com>
2025-09-16 17:37:42 -0400

    Update PrescriptionRepository.java

--- src/main/java/com/example/hospital/service/PrescriptionRepository.java
+++ src/main/java/com/example/hospital/service/PrescriptionRepository.java
@@ -1,10 +1,1 @@
-package com.example.hospital.repository;
 
-import com.example.hospital.model.Prescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-    List<Prescription> findByPatientId(Long patientId);
-}
