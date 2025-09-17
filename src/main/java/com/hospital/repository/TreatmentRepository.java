38690ae8292de65e572b75c46b4062cb85cfcfb2
chris Taylor <ctay456a@gmail.com>
2025-09-17 17:01:21 -0400

    Create TreatmentRepository.java

---
+++ src/main/java/com/hospital/repository/TreatmentRepository.java
@@ -0,0 +1,13 @@
+package com.hospital.repository;
+
+import com.hospital.model.Treatment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
+    List<Treatment> findByDiseaseId(Long diseaseId);
+    List<Treatment> findByPatientId(Long patientId);
+}
