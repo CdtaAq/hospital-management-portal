e5c68368c42d06341636930fb0e30917839a4f02
chris Taylor <ctay456a@gmail.com>
2025-09-19 00:51:48 -0400

    Create RoleRepository.java

---
+++ src/main/java/com/hospital/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.hospital.repository;
+
+import com.hospital.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
